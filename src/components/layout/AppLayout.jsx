7663ecaf48db360fa0f768a276e9964be30e5b5e
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2026-03-30 07:12:48 +0700

    fix: reset scroll position on route change

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../layout/Navbar/Navbar';
 import Footer from '../layout/Footer/Footer';
+import ScrollToTop from '../layout/ScrollToTop';
 import styles from './AppLayout.module.css';
 
 export default function AppLayout({ children }) {
   return (
     <div className={styles.wrapper}>
+      <ScrollToTop />
       <Navbar />
       <main className={styles.main}>
         {children}
